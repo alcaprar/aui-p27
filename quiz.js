81db8d8864a28145bbc1ba5bea960894a65e6c1a
Alessandro Caprarelli <alessandro.caprarelli.ext@siemens.com>
2017-11-17 09:52:38 +0100

    Created first version of a quiz

---
+++ quiz.js
@@ -0,0 +1,22 @@
+var quiz = [
+  {
+    "question": "Question1",
+    "options": [{
+      "option": "Option1.1 (correct)",
+      "isCorrect": true
+    },{
+      "option": "Option1.2",
+      "isCorrect": false
+    }]
+  },
+  {
+    "question": "Question2",
+    "options": [{
+      "option": "Option2.1",
+      "isCorrect": false
+    },{
+      "option": "Option2.2 (correct)",
+      "isCorrect": true
+    }]
+  }
+]
